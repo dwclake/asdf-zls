5020c9ed3925162fb98ca8c43a6a491adb779ae3
dwclake <devonwebb559@gmail.com>
2024-10-04 10:39:14 -0700

    add aweful temporary zls master implementation

--- lib/utils.bash
+++ lib/utils.bash
@@ -76,23 +76,7 @@ get_extname() {
 }
 
 get_version() {
-    echo -n $(awk '
-        BEGIN {
-          FS = "[ /^]+"
-          while ("git ls-remote " "https://github.com/zigtools/zls.git" "| sort -Vk2" | getline) {
-            if (!sha)
-              sha = substr($0, 1, 7)
-            tag = $3
-          }
-          while ("curl -s " "https://github.com/zigtools/zls.git" "/releases/tag/" tag | getline)
-            if ($3 ~ "commits")
-              com = $2
-          major = substr(tag, 0, 1)
-          minor = substr(tag, 3, 6) + 1
-          patch = substr(tag, 7, 8)
-          printf com ? "%d.%d.%d-dev.%s+%s\n" : "%s\n", major, minor, patch, com, sha
-        }' 
-    )
+    echo -n "$(./version.awk)"
 }
 
 download_release() {
